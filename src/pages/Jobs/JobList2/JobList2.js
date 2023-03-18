749f624d67feac2e7f520aee1d77e0cb5aaa8f5e
Shivam Trivedi <strivedi947@gmail.com>
2023-03-18 13:36:09 +0530

    Blog form creation and state complete

--- src/pages/Jobs/JobList2/JobList2.js
+++ src/pages/Jobs/JobList2/JobList2.js
@@ -1,37 +1,23 @@
 import React from 'react'
 import { Col, Container, Row } from 'reactstrap'
-import Section from '../../Jobs/JobList2/Section'
-import JobSearchOptions from '../JobList/JobSearchOptions'
-import Popular from '../JobList/Popular'
-import Sidebar from '../JobList/Sidebar'
 import JobVacancyPost2 from './JobVacancyPost2'
-import Pagination from '../JobList2/Pagination'
-import MetaTags from "react-meta-tags";
 
 const JobList2 = () => {
     return (
         <React.Fragment>
-            <MetaTags>
-            <title>Job List2 | Jobcy - Job Listing Template | Themesdesign</title>
+            {/* <MetaTags>
+                <title>Job List2 | Jobcy - Job Listing Template | Themesdesign</title>
             </MetaTags>
-            <Section/>
+            <Section /> */}
             <section className="section">
-            <Container >
-            <Row>
-            <Col lg={9}>
-            <div className="me-lg-5">
-            <JobSearchOptions/>
-            <Popular/>
-            <JobVacancyPost2/>
-            <Pagination/>
-            </div>            
-            </Col>
-            <Sidebar/>
-            </Row>
-            </Container>
+                <Container >
+                    <div className="me-lg-5">
+                        <JobVacancyPost2 />
+                    </div>
+                </Container>
             </section>
 
-            
+
         </React.Fragment>
     )
 }
